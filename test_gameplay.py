b1963fd828aae369cbadbc1e509df9338e7a1683
goldbocman <hxfcndjd863@gmail.com>
2022-01-22 19:36:12 +0200

    Новые текстуры шариков, пофикшены баги с перезагрузкой стола

--- test_gameplay.py
+++ test_gameplay.py
@@ -36,8 +36,7 @@ else:
 
 parts_dict = {0: 'Твоя линия осадных карт', 1: 'Твоя линия дальников', 2: 'Твоя линия ближников',
               3: 'Линия противника ближников', 4: 'Линия противника дальников', 5: 'Линия противника осадных карт'}
-cards_ids = {1000: ('swoarder1.png', 1)}
-
+cards_ids = {1000: ('swoarder1.png', 'Мечник', 2, 3)}
 x_for_parts_coords, y_for_parts_coords = int(screen_size[0] / 3), screen_size[1] // 9
 parts_coord = {0: (x_for_parts_coords, y_for_parts_coords), 1: (x_for_parts_coords, y_for_parts_coords * 2),
                2: (x_for_parts_coords, y_for_parts_coords * 3), 3: (x_for_parts_coords, y_for_parts_coords * 4),
@@ -76,11 +75,16 @@ class Card(pygame.sprite.Sprite):
         # Это очень важно !!!
         super().__init__(group)
         self.image = load_image(cards_ids[card_id][0])
-        self.image = pygame.transform.scale(self.image, (70, 96))
+
+        self.image = pygame.transform.scale(self.image, (screen_size[0] // 2 // 12, screen_size[1] // 9))
+        # self.image = pygame.transform.scale(self.image, (70, 96))
         self.rect = self.image.get_rect()
         self.rect.centerx, self.rect.centery = 1218, 633
         self.took = False
         self.card_id = card_id
+        self.card_name = cards_ids[card_id][1]
+        self.power = cards_ids[card_id][2]
+        self.card_type = cards_ids[card_id][3]
         self.can_be_placed = True
         self.line = -1
         self.to_pos = (self.rect.centerx, self.rect.centery)
@@ -130,7 +134,10 @@ class Card(pygame.sprite.Sprite):
                     if part:
                         past = list(filter(lambda x: x.get_part_type(args[0]) is not None, [part for part in all_parts]))
                         part = part[0]
-                        len_cards_in_line = len(part.cards) * 70
+                        card_size_x, card_size_y = self.image.get_size()
+                        len_cards_in_line = len(part.cards) * card_size_x
+                        if part.get_part_type(args[0]) != self.card_type:
+                            return
                         self.line = part.get_part_type(args[0])
                         self.took = False
                         self.can_be_placed = False
@@ -149,31 +156,31 @@ class Card(pygame.sprite.Sprite):
                         cards_pos = []
                         if middle_cards == 1:
                             for i in range(1, left_cards + 1):
-                                cards_pos.insert(-1, -70 * i)
+                                cards_pos.insert(-1, -card_size_x * i)
                             cards_pos.append(0)
                             for i in range(1, right_cards + 1):
-                                cards_pos.append(70 * i)
+                                cards_pos.append(card_size_x * i)
                         else:
                             temp = 0
                             for i in range(1, left_cards + 1):
-                                cards_pos.insert(-1, -35 * i + temp)
-                                temp += -35
+                                cards_pos.insert(-1, -(card_size_x // 2) * i + temp)
+                                temp += -(card_size_x // 2)
                             temp = 0
                             for i in range(1, right_cards + 1):
-                                cards_pos.append(35 * i + temp)
-                                temp += 35
+                                cards_pos.append((card_size_x // 2) * i + temp)
+                                temp += (card_size_x // 2)
                         counter = 0
                         for card in part.cards:
                             card.to_pos = [part.rect.centerx + cards_pos[counter], part.rect.centery]
                             counter += 1
                         ball = list(filter(lambda x: x.if_part_type(self.line) is not None, [i for i in balls_stat]))[0]
-                        ball.score += cards_ids[self.card_id][1]
+                        ball.score += self.power
                         if 0 <= self.line <= 2:
                             ball = list(filter(lambda x: x.if_part_type(7) is not None, [ball for ball in balls_stat]))[0]
-                            ball.score += cards_ids[self.card_id][1]
+                            ball.score += self.power
                         elif 3 <= self.line <= 5:
                             ball = list(filter(lambda x: x.if_part_type(6) is not None, [ball for ball in balls_stat]))[0]
-                            ball.score += cards_ids[self.card_id][1]
+                            ball.score += self.power
                         # if len(part.cards) >= 1:
                         #     for card in part.cards:
                         #         card.to_pos = [card.to_pos[0] - 35, card.to_pos[1]]
@@ -204,12 +211,14 @@ class MyPartCards(pygame.sprite.Sprite):
         # Это очень важно !!!
         super().__init__(group)
         self.image = MyPartCards.image_default
+        self.image = pygame.transform.scale(self.image, (screen_size[0] // 2, screen_size[1] // 9))
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = 342, 620
+        self.rect.x, self.rect.y = (x_for_parts_coords, y_for_parts_coords * 7 + int(y_for_parts_coords * 0.5))
         self.my_cards = []
 
     def remove_card(self, card):
         self.my_cards.remove(card)
+        card_size_x, card_size_y = card.image.get_size()
 
         cards_len = len(self.my_cards)
         left_cards = cards_len // 2
@@ -218,19 +227,19 @@ class MyPartCards(pygame.sprite.Sprite):
         cards_pos = []
         if middle_cards == 1:
             for i in range(1, left_cards + 1):
-                cards_pos.insert(-1, -70 * i)
+                cards_pos.insert(-1, -card_size_x * i)
             cards_pos.append(0)
             for i in range(1, right_cards + 1):
-                cards_pos.append(70 * i)
+                cards_pos.append(card_size_x * i)
         else:
             temp = 0
             for i in range(1, left_cards + 1):
-                cards_pos.insert(-1, -35 * i + temp)
-                temp += -35
+                cards_pos.insert(-1, -(card_size_x // 2) * i + temp)
+                temp += -(card_size_x // 2)
             temp = 0
             for i in range(1, right_cards + 1):
-                cards_pos.append(35 * i + temp)
-                temp += 35
+                cards_pos.append((card_size_x // 2) * i + temp)
+                temp += (card_size_x // 2)
         counter = 0
         for card in self.my_cards:
             card.to_pos = [self.rect.centerx + cards_pos[counter], self.rect.centery]
@@ -238,6 +247,7 @@ class MyPartCards(pygame.sprite.Sprite):
 
     def add_card(self, card):
         self.my_cards.append(card)
+        card_size_x, card_size_y = card.image.get_size()
 
         cards_len = len(cards_group)
         left_cards = cards_len // 2
@@ -246,19 +256,19 @@ class MyPartCards(pygame.sprite.Sprite):
         cards_pos = []
         if middle_cards == 1:
             for i in range(1, left_cards + 1):
-                cards_pos.insert(-1, -70 * i)
+                cards_pos.insert(-1, -card_size_x * i)
             cards_pos.append(0)
             for i in range(1, right_cards + 1):
-                cards_pos.append(70 * i)
+                cards_pos.append(card_size_x * i)
         else:
             temp = 0
             for i in range(1, left_cards + 1):
-                cards_pos.insert(-1, -35 * i + temp)
-                temp += -35
+                cards_pos.insert(-1, -(card_size_x // 2) * i + temp)
+                temp += -(card_size_x // 2)
             temp = 0
             for i in range(1, right_cards + 1):
-                cards_pos.append(35 * i + temp)
-                temp += 35
+                cards_pos.append((card_size_x // 2) * i + temp)
+                temp += (card_size_x // 2)
         counter = 0
         for card in self.my_cards:
             card.to_pos = [self.rect.centerx + cards_pos[counter], self.rect.centery]
@@ -301,8 +311,8 @@ class Parts(pygame.sprite.Sprite):
 
 
 class BallsCounters(pygame.sprite.Sprite):
-    image_ball_blue = load_image("ball_blue.png")
-    image_ball_yellow = load_image("ball_yellow.png")
+    image_ball_blue = load_image("blue_ball.png")
+    image_ball_yellow = load_image("yellow_ball.png")
 
     def __init__(self, group, part_type):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
@@ -312,13 +322,15 @@ class BallsCounters(pygame.sprite.Sprite):
         if 0 <= part_type <= 2:
             part = list(filter(lambda x: x.if_part_type(part_type) is not None, [i for i in all_parts]))[0]
             self.image = BallsCounters.image_ball_yellow
+            self.image = pygame.transform.scale(self.image, (part.rect.height // 2, part.rect.height // 2))
             self.rect = self.image.get_rect()
-            self.rect.centerx, self.rect.centery = part.rect.topleft[0], part.rect.topleft[1] + part.rect.height // 2
+            self.rect.centerx, self.rect.centery = part.rect.topleft[0] - part.rect.topleft[0] // 27, part.rect.topleft[1] + part.rect.height // 2
         elif 3 <= part_type <= 5:
             part = list(filter(lambda x: x.if_part_type(part_type) is not None, [i for i in all_parts]))[0]
             self.image = BallsCounters.image_ball_blue
+            self.image = pygame.transform.scale(self.image, (part.rect.height // 2, part.rect.height // 2))
             self.rect = self.image.get_rect()
-            self.rect.centerx, self.rect.centery = part.rect.topleft[0], part.rect.topleft[1] + part.rect.height // 2
+            self.rect.centerx, self.rect.centery = part.rect.topleft[0] - part.rect.topleft[0] // 27, part.rect.topleft[1] + part.rect.height // 2
         elif part_type == 6:
             self.image = BallsCounters.image_ball_blue
             self.rect = self.image.get_rect()
@@ -334,28 +346,38 @@ class BallsCounters(pygame.sprite.Sprite):
             return self
 
     def draw_text(self):
-        font = pygame.font.SysFont('Comic Sans MS', 30)
+        font = pygame.font.Font('our_font.otf', 35)
         text1 = font.render(str(self.score), False,
-                            (154, 48, 48))
-        return text1, (self.rect.centerx * 0.98, self.rect.topleft[1] * 0.99)
+                            (219, 0, 223))
+        return text1, (self.rect.centerx - (self.rect.centerx - self.rect.x) // 2.5, self.rect.y)
 
     # def update(self, *args):
     #     font = pygame.font.SysFont('Comic Sans MS', 30)
     #     text1 = font.render(str(self.score), False,
     #                       (154, 48, 48))
     #     screen.blit(text1, self.rect.center)
+
+v = 1500
+fps = 60
 cards_group = pygame.sprite.Group()
 board = pygame.sprite.Group()
 my_part = pygame.sprite.Group()
 all_parts = pygame.sprite.Group()
 balls_stat = pygame.sprite.Group()
-v = 1500
-fps = 60
 
 
 def play(screen, screen_size):
     # создадим группу, содержащую все спрайты
-
+    global cards_group
+    cards_group = pygame.sprite.Group()
+    global board
+    board = pygame.sprite.Group()
+    global my_part
+    my_part = pygame.sprite.Group()
+    global all_parts
+    all_parts = pygame.sprite.Group()
+    global balls_stat
+    balls_stat = pygame.sprite.Group()
     Card(cards_group, card_id=1000)
     Card(cards_group, card_id=1000)
     Card(cards_group, card_id=1000)
@@ -369,12 +391,10 @@ def play(screen, screen_size):
 
     Table(board)
 
-
     MyPartCards(my_part)
     for my in my_part:
         for card in cards_group:
             my.add_card(card)
-
 
     Parts(all_parts, 0)
     Parts(all_parts, 1)
@@ -382,7 +402,6 @@ def play(screen, screen_size):
     Parts(all_parts, 3)
     Parts(all_parts, 4)
     Parts(all_parts, 5)
-
 
     BallsCounters(balls_stat, 0)
     BallsCounters(balls_stat, 1)
